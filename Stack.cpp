c4ce233a3881799076cbfb56dca1ca0bfd584d37
Md Mominul Islam <124115886+Mominul36@users.noreply.github.com>
2023-05-19 17:49:23 +0600

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int top=-1;
-int capacity=10000;
+
 class Stack
 {
+  int top=-1;
+  int capacity=10000;
   int a[10000];
 public:
   void push(int x);
   void pop();
   void print();
 };
+
 int main()
 {
     Stack s;
@@ -20,9 +22,9 @@ int main()
     s.print();
     s.pop();
     s.print();
-    
+
 }
-Stack :: void push(int x)
+void Stack :: push(int x)
   {
       if(top>=capacity-1)
       cout<< "Stack is full"<<endl;
@@ -33,14 +35,14 @@ Stack :: void push(int x)
       }
 
 }
-Stack :: void pop()
+void Stack :: pop()
 {
        if(top<0)
         cout<< "stack is empty"<<endl;
        else
          top--;
 }
-Stack :: void print()
+void Stack :: print()
 {
       for(int i=0;i<=top;i++)
         cout<<a[i]<< " ";
